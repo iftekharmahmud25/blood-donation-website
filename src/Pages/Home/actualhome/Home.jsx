2b4ab54ddd42c1b12c4cef50dd0282d5453f2674
iftekahrmahmud98@gmail.com <youremail@example.com>
2024-02-07 23:38:16 +0600

    fix the problem

--- src/Pages/Home/actualhome/Home.jsx
+++ src/Pages/Home/actualhome/Home.jsx
@@ -1,5 +1,4 @@
 
-
 import { Helmet } from "react-helmet-async";
 import Banner from "../Banner/Banner";
 import Reviews from "../Reviews/Reviews";
@@ -18,11 +17,11 @@ const Home = () => {
             </Helmet>
             <Banner></Banner>
             <div  className="mt-12">
-                <p className="text-center text-cyan-800 font-semibold  md:text-3xl text-2xl mt-5 mb-6 md:mb-12">--- Our Services ---</p>
+                <p className="text-center text-cyan-800 font-semibold  md:text-3xl text-2xl mt-5 mb-6 md:mb-12"> Our Services </p>
             <ServiceCards></ServiceCards>
             </div>
             <div className="mt-12 mb-12">
-            <p className="text-center text-cyan-800 font-semibold  md:text-3xl text-2xl mt-5 mb-6 md:mb-12">--- Patients Review ---</p>
+            <p className="text-center text-cyan-800 font-semibold  md:text-3xl text-2xl mt-5 mb-6 md:mb-12"> Patients Review </p>
             <Reviews></Reviews>
             </div>
           
